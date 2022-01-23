24b5611e143c3a985d04f18958f129cf3489d0a8
Charlie Xu <xucharliey@gmail.com>
2022-01-23 16:01:24 -0500

    Initial pass at db integration

--- scd40.py
+++ scd40.py
@@ -4,19 +4,49 @@
 import time
 import board
 import adafruit_scd4x
+import mysql.connector
+import os
+from datetime import datetime
+from dotenv import load_dotenv
 
-i2c = board.I2C()
-scd4x = adafruit_scd4x.SCD4X(i2c)
-print("Serial number:", [hex(i) for i in scd4x.serial_number])
+load_dotenv()
 
+scd4x = adafruit_scd4x.SCD4X(board.I2C())
 scd4x.start_periodic_measurement()
 print("Waiting for first measurement....")
 
+cnx = mysql.connector.connect(
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD')
+    )
+
+# Get a cursor
+cur = cnx.cursor()
+
+add_measurement = ("INSERT INTO Measurements "
+                   "(MeasuredAt, CO2, Temp, Humidity) "
+                   "VALUES (%(MeasuredAt)s, %(CO2)s, %(Temp)s, %(Humidity)s)")
+
 while True:
     if scd4x.data_ready:
-        print("CO2: %d ppm" % scd4x.CO2)
-        print("Temperature: %0.1f *C" % scd4x.temperature)
-        print("Humidity: %0.1f %%" % scd4x.relative_humidity)
+        print("Ambient CO2: %d ppm" % scd4x.CO2)
+        print("Sensor Temperature: %0.1f *C" % scd4x.temperature)
+        print("Ambient Humidity: %0.1f %%" % scd4x.relative_humidity)
         print()
+
+        data_measurement = {
+            'MeasuredAt': datetime.combine(datetime.now().date(), datetime.now().time()),
+            'CO2': scd4x.CO2,
+            'Temp': scd4x.temperature,
+            'Humidity': scd4x.relative_humidity,
+        }
+
+        print(add_measurement, data_measurement)
+
+        cur.execute(add_measurement, data_measurement)
+
+        cnx.commit()
     time.sleep(1)
 
